74e06b7f4e87712157171f798b8dd6d56285463c
Karan Rajpal <amigokaran@gmail.com>
2024-07-03 23:24:24 -0400

    Green checkbox

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,8 @@
 with open('.gitignore', 'a') as gitignore_file:
     gitignore_file.write('\n.env.local')
 
+green = "\033[92m"  # ANSI escape code for green color
+reset = "\033[0m"   # ANSI escape code to reset color
+checkbox = "✓"      # Unicode character for checkmark
 
-
-print("Cookiecutter Post-Install Script Completed")
+print(f"Cookiecutter Post-Install Script Completed {green}{checkbox}{reset}")
