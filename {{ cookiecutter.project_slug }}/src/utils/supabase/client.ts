d8f601537f9260c2b9d593fa9e2ba675fe54203f
Karan Rajpal <amigokaran@gmail.com>
2024-07-04 00:15:44 -0400

    supabase typo

--- {{ cookiecutter.project_slug }}/src/utils/supabase/client.ts
+++ {{ cookiecutter.project_slug }}/src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPBASE_PROJECT_URL!,
-    process.env.NEXT_PUBLIC_SUPBASE_API_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_API_KEY!
   )
 }
--- {{ cookiecutter.project_slug }}/src/utils/supabase/middleware.ts
+++ {{ cookiecutter.project_slug }}/src/utils/supabase/middleware.ts
@@ -7,8 +7,8 @@ export async function updateSession(request: NextRequest) {
     })
 
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPBASE_PROJECT_URL!,
-        process.env.NEXT_PUBLIC_SUPBASE_API_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
         {
             cookies: {
                 getAll() {
